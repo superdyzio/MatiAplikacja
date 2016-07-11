20c2f01823fb6088d21cb5d0dfd52ef9a4e8d51f
Dawid Perdek <200450@student.pwr.edu.pl>
2016-07-11 23:55:14 +0200

    initial commit

---
+++ MatiAplikacja/MatiForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace MatiAplikacja
+{
+    partial class MatiForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.progressBarTimeElapsed = new System.Windows.Forms.ProgressBar();
+            this.textBoxTimeRemaining = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonStart.Location = new System.Drawing.Point(189, 177);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(100, 50);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "START";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // progressBarTimeElapsed
+            // 
+            this.progressBarTimeElapsed.Location = new System.Drawing.Point(12, 116);
+            this.progressBarTimeElapsed.Name = "progressBarTimeElapsed";
+            this.progressBarTimeElapsed.Size = new System.Drawing.Size(460, 23);
+            this.progressBarTimeElapsed.TabIndex = 1;
+            // 
+            // textBoxTimeRemaining
+            // 
+            this.textBoxTimeRemaining.Font = new System.Drawing.Font("Microsoft Sans Serif", 29F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxTimeRemaining.Location = new System.Drawing.Point(12, 28);
+            this.textBoxTimeRemaining.Name = "textBoxTimeRemaining";
+            this.textBoxTimeRemaining.Size = new System.Drawing.Size(460, 51);
+            this.textBoxTimeRemaining.TabIndex = 2;
+            this.textBoxTimeRemaining.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MatiForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 262);
+            this.Controls.Add(this.textBoxTimeRemaining);
+            this.Controls.Add(this.progressBarTimeElapsed);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "MatiForm";
+            this.Text = "Mati is the greatest!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ProgressBar progressBarTimeElapsed;
+        private System.Windows.Forms.TextBox textBoxTimeRemaining;
+    }
+}
+
