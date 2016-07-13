e0ca6b8b885c15d09ad3bba70384df5596496f69
Dawid Perdek <200450@student.pwr.edu.pl>
2016-07-14 01:45:34 +0200

    new layout

--- MatiAplikacja/MatiForm.Designer.cs
+++ MatiAplikacja/MatiForm.Designer.cs
@@ -28,26 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.buttonStart = new System.Windows.Forms.Button();
             this.progressBarTimeElapsed = new System.Windows.Forms.ProgressBar();
             this.textBoxTimeRemaining = new System.Windows.Forms.TextBox();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // buttonStart
             // 
             this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.buttonStart.Location = new System.Drawing.Point(189, 177);
+            this.buttonStart.Location = new System.Drawing.Point(75, 178);
             this.buttonStart.Name = "buttonStart";
             this.buttonStart.Size = new System.Drawing.Size(100, 50);
             this.buttonStart.TabIndex = 0;
             this.buttonStart.Text = "START";
             this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
             // progressBarTimeElapsed
             // 
             this.progressBarTimeElapsed.Location = new System.Drawing.Point(12, 116);
+            this.progressBarTimeElapsed.Maximum = 10000;
             this.progressBarTimeElapsed.Name = "progressBarTimeElapsed";
             this.progressBarTimeElapsed.Size = new System.Drawing.Size(460, 23);
+            this.progressBarTimeElapsed.Step = 1;
             this.progressBarTimeElapsed.TabIndex = 1;
             // 
             // textBoxTimeRemaining
@@ -59,11 +65,27 @@
             this.textBoxTimeRemaining.TabIndex = 2;
             this.textBoxTimeRemaining.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
+            // buttonReset
+            // 
+            this.buttonReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonReset.Location = new System.Drawing.Point(315, 178);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(100, 50);
+            this.buttonReset.TabIndex = 3;
+            this.buttonReset.Text = "RESET";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
             // MatiForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 262);
+            this.Controls.Add(this.buttonReset);
             this.Controls.Add(this.textBoxTimeRemaining);
             this.Controls.Add(this.progressBarTimeElapsed);
             this.Controls.Add(this.buttonStart);
@@ -79,6 +101,8 @@
         private System.Windows.Forms.Button buttonStart;
         private System.Windows.Forms.ProgressBar progressBarTimeElapsed;
         private System.Windows.Forms.TextBox textBoxTimeRemaining;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- MatiAplikacja/MatiForm.cs
+++ MatiAplikacja/MatiForm.cs
@@ -12,9 +12,61 @@ namespace MatiAplikacja
 {
     public partial class MatiForm : Form
     {
+        private Boolean run;
+        private Boolean finished;
+        private int seconds;
+        private const int maxTime = 30;
+        // one year = 31 556 926 seconds
+
         public MatiForm()
         {
             InitializeComponent();
+
+            run = false;
+            finished = false;
+            seconds = maxTime;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (run)
+            {
+                seconds--;
+                if (seconds == 0)
+                {
+                    run = false;
+                    finished = true;
+                }
+                progressBarTimeElapsed.Value = (seconds / maxTime);
+            }
+            if (finished)
+            {
+                // TODO: popup
+                timer.Stop();
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (!finished)
+            {
+                run = true;
+                timer.Start();
+            }
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            if (!finished)
+            {
+                run = false;
+                timer.Stop();
+                seconds = maxTime;
+            }
+            else
+            {
+                // TODO: ask if proceed
+            }
         }
     }
 }
