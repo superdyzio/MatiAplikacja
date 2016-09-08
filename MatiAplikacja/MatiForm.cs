60c47342c63118ffda60f8ef4bd308e5a7d44c4e
Dawid <Dyzio@DYZIO-LENOVO>
2016-09-09 00:52:50 +0200

    Revert "Revert "read value from file""
    This reverts commit 66131b2941574bbacc1d64ec84f80796b16dd44d.

--- MatiAplikacja/MatiForm.cs
+++ MatiAplikacja/MatiForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -123,7 +124,19 @@ namespace MatiAplikacja
 
         private void MatiForm_Load(object sender, EventArgs e)
         {
-            Console.WriteLine("Form wczytany");
+            string path = Path.GetDirectoryName(Application.ExecutablePath) + "\\MatiAplikacja_data\\value.txt";
+            try
+            {   
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    String line = sr.ReadToEnd();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
         }
     }
 }
